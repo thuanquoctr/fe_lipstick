e1308959d5c4b0f1dd061807eae8735377ec794e
thuanquoctr <thuanquoctr@gmail.com>
2024-10-23 08:54:32 +0700

    ds fe

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,54 +1,53 @@
-import Rating from "@mui/material/Rating";
-import Typography from "@mui/material/Typography";
 import { useState } from "react";
 const Card = () => {
   const [value, setValue] = useState(2);
   return (
-    <div
-      className="single_advisor_profile wow fadeInUp"
-      data-wow-delay="0.2s"
-      style={{
-        visibility: "visible",
-        animationDelay: "0.2s",
-        animationName: "fadeInUp",
-      }}
-    >
-      {/* Team Thumb */}
-      <div className="advisor_thumb">
-        <img
-          style={{ width: "100%" }}
-          src="https://images.unsplash.com/photo-1596462502278-27bfdc403348?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bGlwc3RpY2t8ZW58MHx8MHx8fDA%3D"
-          alt=""
-        />
-        {/* Social Info */}
-        <div className="social-info">
-          <a href="#">
-            <i className="fa fa-facebook"></i>
-          </a>
-          <a href="#">
-            <i className="fa fa-twitter"></i>
-          </a>
-          <a href="#">
-            <i className="fa fa-linkedin"></i>
-          </a>
-        </div>
-      </div>
-      {/* Team Details */}
-      <div className="single_advisor_details_info">
-        <h6>
-          <i class="bi bi-bag-plus fs-4"></i>
-          <i class="bi bi-eye fs-4 mx-2"></i>
-          <i class="bi bi-suit-heart fs-4"></i>
-        </h6>
+    // <div
+    //   className="single_advisor_profile wow fadeInUp"
+    //   data-wow-delay="0.2s"
+    //   style={{
+    //     visibility: "visible",
+    //     animationDelay: "0.2s",
+    //     animationName: "fadeInUp",
+    //   }}
+    // >
+    //   {/* Team Thumb */}
+    //   <div className="advisor_thumb">
+    //     <img
+    //       style={{ width: "100%" }}
+    //       src="https://images.unsplash.com/photo-1596462502278-27bfdc403348?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bGlwc3RpY2t8ZW58MHx8MHx8fDA%3D"
+    //       alt=""
+    //     />
+    //     {/* Social Info */}
+    //     <div className="social-info">
+    //       <a href="#">
+    //         <i className="fa fa-facebook"></i>
+    //       </a>
+    //       <a href="#">
+    //         <i className="fa fa-twitter"></i>
+    //       </a>
+    //       <a href="#">
+    //         <i className="fa fa-linkedin"></i>
+    //       </a>
+    //     </div>
+    //   </div>
+    //   {/* Team Details */}
+    //   <div className="single_advisor_details_info">
+    //     <h6>
+    //       <i class="bi bi-bag-plus fs-4"></i>
+    //       <i class="bi bi-eye fs-4 mx-2"></i>
+    //       <i class="bi bi-suit-heart fs-4"></i>
+    //     </h6>
 
-        <Rating
-          className="titlereview"
-          name="read-only"
-          value={value}
-          readOnly
-        />
-      </div>
-    </div>
+    //     <Rating
+    //       className="titlereview"
+    //       name="read-only"
+    //       value={value}
+    //       readOnly
+    //     />
+    //   </div>
+    // </div>
+    <>thuan</>
   );
 };
 export default Card;
